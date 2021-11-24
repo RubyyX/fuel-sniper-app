c0d346a03be5b8a58a081dd91096575d554c8313
Ethan Colgan <ethancolgan@adam.com.au>
2021-11-24 16:58:16 +1030

    Initial Commit

---
+++ Fuel Sniper/Fuel_SniperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fuel_SniperApp.swift
+//  Fuel Sniper
+//
+//  Created by Ethan Colgan on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Fuel_SniperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
