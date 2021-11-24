55bab56ff5f122029cf8ec57e8258f15d55a0991
jklhk <wer>
2021-11-24 18:45:46 +1030

    Rating

--- Fuel Sniper/ContentView.swift
+++ Fuel Sniper/ContentView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State var circleProgress: CGFloat = 1
     @State var avgPrice: CGFloat = 40.26
+    
+    var ratings = ["Bad", "Average", "Good"]
+  
+    
     var body: some View {
         VStack(){
             //The guage and reading of avg city price
@@ -18,14 +22,14 @@ struct ContentView: View {
                 Circle()
                     .trim(from: 0.0, to: circleProgress/4*3)
                     .stroke(Color.blue, lineWidth: 15)
-                    .frame(width: 200, height: 200)
+                    .frame(width: 150, height: 150)
                     .rotationEffect(Angle(degrees: -225))
                 Text("\(Int(self.avgPrice))")
                     .font(.custom("HelveticaNeue", size: 40.0))
             }
             .padding(.top, 100)
-                
-            Text("Good")
+            //rating based on circle progress
+            Text(ratings[Int(circleProgress*3)-1])
                 
             Divider()
             Spacer()
