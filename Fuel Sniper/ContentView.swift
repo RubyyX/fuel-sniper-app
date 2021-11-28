adfc86202a54460d60badae9c079fa7da0900ab1
jklhk <wer>
2021-11-28 21:40:28 +1030

    Putting graph into cell

--- Fuel Sniper/ContentView.swift
+++ Fuel Sniper/ContentView.swift
@@ -30,14 +30,14 @@ private func getTimeLabels() -> [String] {
 }
 
 struct ContentView: View {
-    @State var circleProgress: CGFloat = 0.3
+    @State var circleProgress: CGFloat = 0.6
     @State var avgPrice: CGFloat = 40.26
     @State private var selectedFuelType = "Unleaded 95"
     // animation value
     @State private var animationAmount: CGFloat = 0
     
     var primaryColor = Color(red: 0 / 255, green: 100 / 255, blue: 156 / 255)
-    var ratings = ["Bad", "Average", "Good"]
+    var ratings = ["Expensive", "Average", "Cheap"]
     let fuelTypes = ["Unleaded 95", "Unleaded 91", "Deisel"]
 
     var body: some View {
@@ -64,73 +64,85 @@ struct ContentView: View {
                 }
             }
             .background(RoundedRectangle(cornerRadius: 10).fill(primaryColor))
-            .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+            .padding(EdgeInsets(top: 5, leading: 5, bottom: 0, trailing: 5))
             
-            //The guage and reading of avg city price
-            ZStack()
-            {
-                //grey background circle
-                Circle()
-                    .trim(from: 0.0, to: 1 / 4*3)
-                    .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
-                    .foregroundColor(Color(UIColor.white))
-                    .frame(width: 150, height: 150)
-                    .rotationEffect(Angle(degrees: -225))
-                   
-                //guage circle
-                Circle()
-                    .trim(from: 0.0, to: animationAmount*(circleProgress / 4*3))
-                    .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
-                    .foregroundColor(Color(UIColor(hue: CGFloat(circleProgress) / 3, saturation: 1.0, brightness: 1.0, alpha: 1.0)))
+            // middle Panel
+            VStack {
+                // makes width fill
+                HStack {
+                    Spacer()
+                }
+                // guage title
+                Text("Today's Average Price AUD")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
                 
-                    .frame(width: 150, height: 150)
-                    .rotationEffect(Angle(degrees: -225))
+                // The guage and reading of avg city price
+                ZStack {
+                    // grey background circle
+                    Circle()
+                        .trim(from: 0.0, to: 1 / 4*3)
+                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                        .foregroundColor(Color(UIColor.white))
+                        .frame(width: 150, height: 150)
+                        .rotationEffect(Angle(degrees: -225))
+                       
+                    // guage circle
+                    Circle()
+                        .trim(from: 0.0, to: animationAmount*(circleProgress / 4*3))
+                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                        .foregroundColor(Color(UIColor(hue: CGFloat(circleProgress) / 3, saturation: 1.0, brightness: 1.0, alpha: 1.0)))
                     
+                        .frame(width: 150, height: 150)
+                        .rotationEffect(Angle(degrees: -225))
+                        
+                    // average price text
+                    Text("\(Int(self.avgPrice))")
+                        .font(.custom("HelveticaNeue", size: 40.0))
+                        .foregroundColor(.white)
+                        .bold()
+                }
+                .padding(.top, 0)
                 
-                //average price text
-                Text("\(Int(self.avgPrice))")
-                    .font(.custom("HelveticaNeue", size: 40.0))
+                // rating based on circle progress
+                if circleProgress == 0 {
+                    Text(ratings[0])
+                        .foregroundColor(.white)
+                }
+                else {
+                    Text(ratings[Int(ceil(circleProgress*3)) - 1])
+                        .foregroundColor(.white)
+                        .font(.system(size: 25))
+                }
             }
-            .padding(.top, 100)
-        
-            //rating based on circle progress
-            if circleProgress == 0
-            {
-                Text(ratings[0])
-            }
-            else
-            {
-                Text(ratings[Int(ceil(circleProgress*3))-1])
-            }
+            .background(RoundedRectangle(cornerRadius: 10).fill(primaryColor))
+            .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
             
-            
-            //Divider()
-            
-            let prices = getFuelPrices().map { Int($0.price) } //Prices array
-            let labels = getTimeLabels() //Labels array
-            
-            LineChartView(values: prices, labels: labels) //Display graph by calling function
-            
-            Spacer() //Space after the graph
-        }
-        
-        .background(Color.green)
-            .edgesIgnoringSafeArea(.all)
-            .onAppear
-            {
-                //appear animation animationamount is CGFloat 0-1
-                withAnimation(Animation.easeOut(duration: 1))
-                {
+            .onAppear {
+                // appear animation animationamount is CGFloat 0-1
+                withAnimation(Animation.easeOut(duration: 1)) {
                     animationAmount = 1
                 }
             }
             .onDisappear {
                 animationAmount = 0
             }
-    
+            
+            //3rd panel "graph"
+            VStack(){
+            let prices = getFuelPrices().map { Int($0.price) } //Prices array
+            let labels = getTimeLabels() //Labels array
+            
+            LineChartView(values: prices, labels: labels) //Display graph by calling function
+            }
+            .background(RoundedRectangle(cornerRadius: 10).fill(primaryColor))
+            .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
+            
             Spacer()
         }
     }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
